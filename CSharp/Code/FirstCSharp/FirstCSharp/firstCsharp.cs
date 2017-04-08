40dff7d892e1d4c10afed785323e26688d917979
lionel Shen <successwolf@163.com>
2017-04-08 18:58:40 +0800

    firstCsharpCode
    firstCsharpCode

---
+++ CSharp/Code/FirstCSharp/FirstCSharp/firstCsharp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstCSharp
+{
+    class firstCsharp
+    {
+        static void Main(string[] args)   // static
+        {
+            if (args.Length == 2)
+            {
+                System.Console.WriteLine("args.Length is 2");
+            }
+            else
+            {
+                Console.WriteLine("args.Lenght is not 2 ");
+            }
+
+            Console.WriteLine("parameter count = {0}", args.Length);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+
+            foreach (string s in args)  // IEnumerable接口
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Please enter your name:");
+            Console.ReadLine();//读取用户输入
+            Console.WriteLine("Welcome to you !");
+            
+        }
+    }
+}
